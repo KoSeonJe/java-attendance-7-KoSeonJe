aba8c6875e941b26cb786cc34fce57da4555ffae
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2024-12-14 14:11:50 +0900

    feat: 날짜로 요일을 판단한다

---
+++ src/main/java/attendance/service/DayOfWeekService.java
@@ -0,0 +1,24 @@
+package attendance.service;
+
+import static attendance.common.Month.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DayOfWeekService {
+
+    private final Map<Integer, String> dayOfWeek12 = new HashMap<>();
+
+    public DayOfWeekService() {
+        dayOfWeek12.put(2, MONDAY.getDescription());
+        dayOfWeek12.put(3, TUESDAY.getDescription());
+        dayOfWeek12.put(4, WEDNESDAY.getDescription());
+        dayOfWeek12.put(5, THURSDAY.getDescription());
+        dayOfWeek12.put(6, FRIDAY.getDescription());
+    }
+
+    public String getDayOfWeek(int day) {
+        int dayOfWeek = day % 7;
+        return dayOfWeek12.get(dayOfWeek);
+    }
+}
